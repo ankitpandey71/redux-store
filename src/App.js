101fa4d4b6ff7f02c0d4651f26517b3d6075fc89
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-04 17:38:03 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <span className="logo">Redux Store</span>
+    </div>
+  );
+};
+
+export default Navbar;
